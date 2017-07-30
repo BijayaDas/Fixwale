4a623ba6ea0705610f4585cbcd5d2c5b2c1df012
= <bkdas@teamdrg.com>
2017-07-31 00:45:48 +0530

    todos.

--- app/models/connecting_services/enquiry.rb
+++ app/models/connecting_services/enquiry.rb
@@ -1,3 +1,15 @@
 class ConnectingServices::Enquiry < ApplicationRecord
 	self.table_name = 'enquiries'
+
+	acts_as_paranoid
+	belongs_to :category
+	belongs_to :user
+
+	has_many :replies, dependent: :destroy
+
+	enum status: [:open, :closed]
+
+	def enquiry_owner(user)
+		self.user_id == user.id
+	end
 end
